33679b2fb07dc6c2ec00da7efc28c19a5dc9e4c4
SecretPasta <boris.shoostin@gmail.com>
2023-08-26 21:04:37 +0300

    Added a Start Game UI Widget

--- Source/ToonTanks/ToonTanksGameMode.cpp
+++ Source/ToonTanks/ToonTanksGameMode.cpp
@@ -32,6 +32,9 @@ void AToonTanksGameMode::BeginPlay() {
 void AToonTanksGameMode::HandleGameStart() {
 	Tank = Cast<ATank>(UGameplayStatics::GetPlayerPawn(this, 0));
 	ToonTanksPlayerController = Cast<AToonTanksPlayerController>(UGameplayStatics::GetPlayerController(this, 0));
+
+	StartGame();
+
 	if (ToonTanksPlayerController) {
 		ToonTanksPlayerController->SetPlayerEnabledState(false);
 
--- Source/ToonTanks/ToonTanksGameMode.h
+++ Source/ToonTanks/ToonTanksGameMode.h
@@ -22,6 +22,9 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	UFUNCTION(BlueprintImplementableEvent)
+	void StartGame();
+
 private:
 
 	class ATank* Tank;
